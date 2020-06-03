a0ab66b37e97f9c63d1ddd688075de0d497bb663
M Yousif Channa <channayousif@users.noreply.github.com>
2020-06-03 13:42:21 +0500

    My first edit of the app

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        This is a Sample Edit by Yousif Channa
         </p>
-        <div> This is a Sample Edit by Yousif Channa</div>
-        <a
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
