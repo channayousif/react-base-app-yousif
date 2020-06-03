452ced2fd2591d67a485df20b5090d1e348ec940
M Yousif Channa <channayousif@users.noreply.github.com>
2020-06-03 06:09:14 +0500

    My first edit of the app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div> This is a Sample Edit by Yousif Channa</div>
         <a
           className="App-link"
           href="https://reactjs.org"
